14b246ffdf1df91c19f2a6f7e775b76b071521ff
heri <90657643+halarcont@users.noreply.github.com>
2023-06-03 01:33:54 -0400

    Delete 2_springboot-servicio[base_sin_eureka] directory

--- 2_springboot-servicio[base_sin_eureka]/springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/dao/ProductoDao.java
+++
@@ -1,9 +0,0 @@
-package com.formacionbdi.springboot.app.productos.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.formacionbdi.springboot.app.productos.models.entity.Producto;
-
-public interface ProductoDao extends CrudRepository<Producto, Long>{
-
-}
